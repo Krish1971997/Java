53142134cbdbbea5003e7955cceac9e214bfee87
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-15 10:20:17 +0530

    Update Zprogram_a10b2c5.java

--- Java/Zprogram_a10b2c5.java
+++ Java/Zprogram_a10b2c5.java
@@ -1,1 +1,36 @@
 
+class Zprogram_a10b2c5
+{
+  public static void main (String[]args)
+  {
+    String s = "a10b3c5";
+    String letter = "";
+    int j = 0;int digit=0;
+    for (int i = 0; i < s.length (); i++)
+      {
+	if (Character.isAlphabetic(s.charAt (i)))
+	  {
+	    letter += s.charAt (i);
+	    j++;
+
+	  }
+	else
+	  digit = Character.getNumericValue(s.charAt (i));
+	if (i + 1 < s.length ())
+	  {
+	    if (Character.isDigit (s.charAt(i + 1)))
+	      {
+		String a = String.valueOf(digit);
+		a += s.charAt (i + 1);
+		digit = Integer.parseInt (a);
+	      }
+	  }
+	for (int k = 0; k < digit; k++)
+	  {
+	    System.out.print (letter.charAt(j));
+	  }
+
+      }
+
+  }
+}
