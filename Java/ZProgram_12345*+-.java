1c34055133f470735d795af720722939d6ca52d9
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-15 21:05:46 +0530

    Update ZProgram_12345*+-.java

--- Java/ZProgram_12345*+-.java
+++ Java/ZProgram_12345*+-.java
@@ -1,23 +1,30 @@
 public class ZProgram_12345*+-{
-public static void main(String[] args){
-String s="12345 * + - + "; 
-int j=0,k=0,sum=0;
-int x=0;y=0
-String sp[]=new int[s.length()/2];
-int arr[]=new int[s.length()/2+1];
-for(int i=0;i<s.length();i++){
-if(Character.isDigit(s.charAt(i))){
-arr[j++]=Character.getNumericValue(s.charAt(i)));
-}
-else
-sp[k++]=Character.getNumericValue(s.charAt(i)));
+public static void main(String[] args) {
+		String s = "374291--*+-", sp="";
+		int arr[] = new int[s.length() / 2 + 1];
+		int j = 0, sum = 0,  x = 1, y = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				arr[j++] = Character.getNumericValue(s.charAt(i));
+			} else
+				sp += (s.charAt(i));
+		}
+		sum = arr[0];
+		for (int i = 0; i < sp.length(); i++) {
+			sum = calculation(sum, arr[x++], sp.charAt(y++));
+		}
+		System.out.println("Sum ---> "+sum);
+	}
 
-}
-sum=calculation(sum,arr[x]);
-for(int i=0;i<s.length();i++){
-sum=calculation(sum,arr[x],sp[y]);
-}
-}
-public static int calculation(){
-}
+	public static int calculation(int summ, int arr, char c) {
+		int sum=0;
+		switch (c) {
+		case '+': sum=summ+arr; return sum;
+		case '-': sum=summ-arr; return sum;
+		case '*': sum=summ*arr; return sum;
+		case '/': sum=summ/arr; return sum;
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + c);
+    }		
+	}
 }
