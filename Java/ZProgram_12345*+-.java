dedb95a3d16e7da9a2c7fa2d208db6ff7eeb3f48
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-15 17:04:53 +0530

    Update ZProgram_12345*+-.java

--- Java/ZProgram_12345*+-.java
+++ Java/ZProgram_12345*+-.java
@@ -1,1 +1,23 @@
+public class ZProgram_12345*+-{
+public static void main(String[] args){
+String s="12345 * + - + "; 
+int j=0,k=0,sum=0;
+int x=0;y=0
+String sp[]=new int[s.length()/2];
+int arr[]=new int[s.length()/2+1];
+for(int i=0;i<s.length();i++){
+if(Character.isDigit(s.charAt(i))){
+arr[j++]=Character.getNumericValue(s.charAt(i)));
+}
+else
+sp[k++]=Character.getNumericValue(s.charAt(i)));
 
+}
+sum=calculation(sum,arr[x]);
+for(int i=0;i<s.length();i++){
+sum=calculation(sum,arr[x],sp[y]);
+}
+}
+public static int calculation(){
+}
+}
