2ad9194562dca8b3a3a7518a689339027b872ba2
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-16 17:36:31 +0530

    Update FirstMAXSecondMIN.java

--- Java/FirstMAXSecondMIN.java
+++ Java/FirstMAXSecondMIN.java
@@ -1,4 +1,6 @@
-public class FirstMAXSecondMIN
+import java.util.Arrays;
+
+public class FirstMAXSecondMIN{
 public static void main(String[] args){
 int[] num={1,2,3,4,5,6,7};
 int length=num.length;
@@ -8,11 +10,12 @@ int length=num.length;
 Arrays.sort(num);
 for(int i=0,max=length-1,min=0;i<num.length;i++){
 if(i%2==0){
-System.out.print(num[max]+",");
+System.out.print(num[max--]+",");
 }
 else if(i%2==1){
 System.out.print(num[min++]+",");
 }
 }
 
+}
 }
