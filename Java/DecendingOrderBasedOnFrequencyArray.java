031dbbca0236845d25fe4edd9e46a506a427a461
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-18 18:38:52 +0530

    Update DecendingOrderBasedOnFrequencyArray.java

--- Java/DecendingOrderBasedOnFrequencyArray.java
+++ Java/DecendingOrderBasedOnFrequencyArray.java
@@ -1,29 +1,30 @@
-public class DecendingOrderBasedOnFrequencyArray
-{
+import java.util.HashMap;
+import java.util.Map;
+
+public class DecendingOrderBasedOnFrequencyArray {
   public static void main (String args[])
   {
-    int input = { 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 };
-    int res = new int[input.length];
+    int[] input = { 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 };
+    int[] res = new int[input.length];
     Map < Integer, Integer > map = new HashMap ();
     int count = 1;
     for (int i = 0; i < input.length; i++)
       {
 	for (int j = i + 1; j < input.length; j++)
 	  {
-	    if (input[i] = input[j])
+	    if (input[i] == input[j])
 	      {
 		count++;
 	      }
 	  }
 	map.put (input[i], count);
       }
-  }
-for (Map.Entry m:map.enrtySet ())
+      for(Map.Entry m:map.entrySet())
     {
-      for (int i = 0; i < m.getValue (); i++)
-	{
-	  System.out.print (m.getKey () + " ");
-	}
+        for (int i = 0; i < m.getValue(); i++) {
+            System.out.print(m.getKey() + " ");
+        }
     }
-}
+  }
+    
 }
