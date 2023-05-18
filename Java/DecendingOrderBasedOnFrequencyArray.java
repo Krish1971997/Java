9a3491b4a5c9d5a176da07db67fc3b29c7acee1e
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-18 18:02:00 +0530

    Update DecendingOrderBasedOnFrequencyArray.java

--- Java/DecendingOrderBasedOnFrequencyArray.java
+++ Java/DecendingOrderBasedOnFrequencyArray.java
@@ -1,25 +1,29 @@
-
 public class DecendingOrderBasedOnFrequencyArray
 {
-  public static void main(String args[]){
-int input= {2,2,3,4,5,12,2,3,3,3,12};
-int res=new int[input.length];
-Map<Integer, Integer> map=new HashMap();
-int count=1;
-for(int i=0;i<input.length;i++){
-for(int j=i+1;j<input.length;j++){
-if(input[i]=input[j]){
-count++;
+  public static void main (String args[])
+  {
+    int input = { 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 };
+    int res = new int[input.length];
+    Map < Integer, Integer > map = new HashMap ();
+    int count = 1;
+    for (int i = 0; i < input.length; i++)
+      {
+	for (int j = i + 1; j < input.length; j++)
+	  {
+	    if (input[i] = input[j])
+	      {
+		count++;
+	      }
+	  }
+	map.put (input[i], count);
+      }
+  }
+for (Map.Entry m:map.enrtySet ())
+    {
+      for (int i = 0; i < m.getValue (); i++)
+	{
+	  System.out.print (m.getKey () + " ");
+	}
+    }
 }
 }
-map.put(input[i],count);
-}
-}
-for(Map.Entry m:map.enrtySet()){
-for(int i=0;i<m.getValue();i++){
-System.out.print(m.getKey()+" ");
-}
-}
-}
-}
-
