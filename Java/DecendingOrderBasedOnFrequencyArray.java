5daf67ad3bc7a3ebacd78b815989e6da4d8038f4
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-18 23:45:51 +0530

    Update DecendingOrderBasedOnFrequencyArray.java
    Need to correct

--- Java/DecendingOrderBasedOnFrequencyArray.java
+++ Java/DecendingOrderBasedOnFrequencyArray.java
@@ -1,30 +1,50 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class DecendingOrderBasedOnFrequencyArray {
-  public static void main (String args[])
-  {
-    int[] input = { 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 };
-    int[] res = new int[input.length];
-    Map < Integer, Integer > map = new HashMap ();
-    int count = 1;
-    for (int i = 0; i < input.length; i++)
-      {
-	for (int j = i + 1; j < input.length; j++)
-	  {
-	    if (input[i] == input[j])
-	      {
-		count++;
-	      }
-	  }
-	map.put (input[i], count);
-      }
-      for(Map.Entry m:map.entrySet())
-    {
-        for (int i = 0; i < m.getValue(); i++) {
-            System.out.print(m.getKey() + " ");
-        }
-    }
-  }
-    
+public class DecendingOrderBasedOnFrequencyArray implements Comparable<Map.Entry<Integer, Integer>> {
+	public static void main(String args[]) {
+	
+		int[] input = { 2, 2, 3, 4, 5, 12, 2, 3, 3, 3, 12 };
+		int[] res = new int[input.length];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int count = 1;
+		for (int i = 0; i < input.length; i++) {
+			count=1;
+			for (int j = i + 1; j < input.length; j++) {
+				if ((input[i] == input[j])&&(input[i]!=0)) {
+					count++;
+					input[j]=0;
+				}
+			}
+			if(input[i]!=0)
+				map.put(input[i], count);
+			}
+	/*	Map<Integer, Integer> byValue=map.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, 
+						Map.Entry::getValue, (e1,e2) -> e1,LinkedHashMap::new));
+		*/	for (Map.Entry m : map.entrySet()) {
+			int num=(int) m.getValue();
+			for (int i = 0; i < num; i++) 
+			{
+				System.out.print(m.getKey() + " ");
+			}
+		}
+	}
+
+	public int compareTo(Map.Entry<Integer, Integer> o) {
+		return o.getKey().compareTo(hashCode());
+	}
+
+/*class ValueCompare implements Comparator<Map.Entry<Integer, Integer>> {
+
+	@Override
+	public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+		if(o1.getValue()==o2.getValue())
+			return 0;
+		else if(o1.getValue()>o2.getValue()) 
+			return 1;
+		else
+			return -1;
+	}*/
 }
+
