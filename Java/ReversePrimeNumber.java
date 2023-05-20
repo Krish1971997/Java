3404003620be63d1ecbfd5a454fea890b1431b65
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-20 14:32:54 +0530

    Update ReversePrimeNumber.java

--- Java/ReversePrimeNumber.java
+++ Java/ReversePrimeNumber.java
@@ -1,1 +1,38 @@
+public class ReversePrimeNumber {
+    public static void main(String[] args) {
+        int num = 23;
+        boolean check = primeCheck(num);
+        if (check) {
+            int reverse = ReverseNumber(num);
+            boolean twistedNum = primeCheck(reverse);
+            if (twistedNum) {
+                System.out.println(num + " is twisted prime number");
+                return;
+            } else
+                System.out.println(num + " is not twisted prime number");
 
+            System.out.println("Prime number but not twisted prime number");
+        } else
+            System.out.println("Not Prime number");
+    }
+
+    private static int ReverseNumber(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum = sum * 10 + num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    private static boolean primeCheck(int num) {
+        boolean flag = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
