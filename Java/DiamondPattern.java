e02bfad70f2b17ef3de0da64cc5dc4df28a930c8
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-20 15:00:08 +0530

    Update DiamondPattern.java

--- Java/DiamondPattern.java
+++ Java/DiamondPattern.java
@@ -1,3 +1,17 @@
 public class DiamondPattern {
-    
+    public static void main(String[] args) {
+        int num = 3;
+        int n = num + num - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (num * num <= i + j)
+                    System.out.print("*");
+                // else if (n != 0)
+                // System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
