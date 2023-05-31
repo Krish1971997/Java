aa9c0632be96dec305ea7e4fd1a956780b71a44f
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-05-31 18:15:08 +0530

    Update LeetcodeMedianofTwoSortedArrays.java

--- Java/LeetcodeMedianofTwoSortedArrays.java
+++ Java/LeetcodeMedianofTwoSortedArrays.java
@@ -1,25 +1,24 @@
+import java.util.Arrays;
 
 public class LeetcodeMedianofTwoSortedArrays {
-public static void main(String[] args){
-int nums1 [] = {1,3}, nums2[] = {2};
-int mergedArray=new int[nums1+nums2];
-double res=0.00000;
-for(int i=0,j=0,k=0;i<mergedArray.length;i++){
-if(nums1.length>i)
-mergedArray[j++]=nums[i];
-else
-mergedArray[j++]=nums[k++];
-}
-Arrays.sort(mergedArray);
-if(mergedArray.length%2==0){
-int divide=mergedArray.length/2;
-res=(mergedArray[divide]+mergedArray[divide-1])/2;
-
-}else{
-int divide=mergedArray.length/2;
-res=mergedArray[divide];
-}
-System.out.println("Result ---> "+res);
-
-}    
+    public static void main(String[] args) {
+        int nums1[] = { 1, 2 }, nums2[] = { 3, 4 };
+        int mergedArray[] = new int[nums1.length + nums2.length];
+        double res = 0.00000;
+        for (int i = 0, j = 0, k = 0; i < mergedArray.length; i++) {
+            if (nums1.length > i)
+                mergedArray[j++] = nums1[i];
+            else
+                mergedArray[j++] = nums2[k++];
+        }
+        Arrays.sort(mergedArray);
+        if (mergedArray.length % 2 == 0) {
+            int divide = mergedArray.length / 2;
+            res = (double) (mergedArray[divide] + mergedArray[divide - 1]) / 2;
+        } else {
+            int divide = mergedArray.length / 2;
+            res = mergedArray[divide];
+        }
+        System.out.println("Result ---> " + res);
+    }
 }
