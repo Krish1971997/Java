f53c2295e96800b19c10cbc479a959b070498f7e
Krish1971997 <118267997+Krish1971997@users.noreply.github.com>
2023-06-01 11:48:54 +0530

    Update ANotes.java

--- Java/ANotes.java
+++ Java/ANotes.java
@@ -5,6 +5,9 @@ https://www.geeksforgeeks.org/word-break-problem-dp-32/
 
 https://krish1971997-psychic-broccoli-r94jqp4pwx62p6x.github.dev/?autoStart=false
 
+//C output questions
+https://www.geeksforgeeks.org/output-c-programs-set-59-loops-control-statements/
+
 
 import java.util.HashSet;
 import java.util.List;
